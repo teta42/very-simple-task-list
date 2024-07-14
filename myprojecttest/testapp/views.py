467e698d8c4d5086c788db997ba650b38c351a22
teta42 <nerdsinc42@gmail.com>
2024-07-14 13:16:26 +0500

    чистка

--- myprojecttest/testapp/views.py
+++ myprojecttest/testapp/views.py
@@ -1,24 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.shortcuts import render
 import json
 from django.http import HttpResponse
 
-def home(request):
-    if request.user.is_authenticated:
-        return render(request, 'yes_reg.html', {'username': request.session.get('username', None)})
-    else:
-        return render(request, 'no_reg.html')
 
-def reg(request):
-    if request.method == 'GET':
-        return render(request, 'reg.html')
-    elif request.method == 'POST':
-        json_data = json.loads(request.body)
-        username = json_data["username"]
-        password = json_data["password"]
-        user = User.objects.create_user(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            request.session['username'] = username
-            return HttpResponse('True')
